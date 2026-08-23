b432f7258330396df3c4cbd1fb490adae8ffa6b8
alejandrocolombani <alejandrojcolombani@gmail.com>
2026-08-22 23:29:05 -0400

    fixed task 9

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -33,13 +33,15 @@ export class StudentHolberton {
   }
 }
 
-export const class2019 = new HolbertonClass(2019, 'San Francisco');
-export const class2020 = new HolbertonClass(2020, 'San Francisco');
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-export const student1 = new StudentHolberton(
-  'Guillaume',
-  'Salva',
-  class2020,
-);
-export const student2 = new StudentHolberton('John', 'Doe', class2020);
-export const student3 = new StudentHolberton('Albert', 'Cloning', class2019);
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+
+const listOfStudents = [student1, student2, student3, student4, student5];
+
+export default listOfStudents;
